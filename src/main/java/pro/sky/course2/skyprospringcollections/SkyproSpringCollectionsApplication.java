20c175f08fa4e7cb7103bf70a3fc1383be58f9de
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-07-21 09:00:17 +0300

    hw

---
+++ src/main/java/pro/sky/course2/skyprospringcollections/SkyproSpringCollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.skyprospringcollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringCollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproSpringCollectionsApplication.class, args);
+    }
+
+}
